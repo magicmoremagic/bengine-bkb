bf8d7a178fa9dd2de81d311d087fa8286a26ef63
Ben Crist <ben@magicmoremagic.com>
2018-09-01 19:55:00 -0500

    initial commit

---
+++ build.lua
@@ -0,0 +1,11 @@
+demo 'bkb' {
+   app {
+      icon 'icon/bengine.ico',
+      limp_src 'src/*.hpp',
+      src 'src/*.cpp',
+      link_project {
+         'core',
+         'core-id'
+      }
+   }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+## BKB 'Driver'
+For instructions on building, see the main repository at
+[magicmoremagic/bengine](https://github.com/magicmoremagic/bengine).
